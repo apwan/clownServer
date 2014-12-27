462c5f98514b6daf01066569cf5ccc8075bc9f78
lzn <linzinan1995@126.com>
2014-12-27 22:57:53 +0800

    add settings.js, update db,js

--- ctrl/db.js
+++ ctrl/db.js
@@ -2,13 +2,27 @@
  * Created by Wu Yijie on 12/26/14.
  */
 
+var settings = require('./settings');
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
 
 var db = {
 
     version: '0.0.0',
-    test: function(){
-        return 'database' + this.version;
-    }
+	
+	database : new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT, {})),
+	
+	ClearDatabase: function(callback){
+		var collectionList = ["users", "slides", "resources"];
+		for (var i = 0; i < collectionList; i++){
+			database.dropCollection(collectionList[i], function(err, result){
+				if (err) {
+					return callback(err);
+				}
+			})
+		}
+	}	
 }
 
 exports.db = db;
---
+++ ctrl/settings.js
@@ -0,0 +1,4 @@
+module.exports = {
+	db: 'clown',
+	host: 'localhost',
+};
