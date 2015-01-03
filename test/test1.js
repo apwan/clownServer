8feebbbde3c25250cba925287b7f3ff8fc16bd39
lzn <linzinan1995@126.com>
2015-01-03 15:41:22 +0800

    test-test1

--- test/test1.js
+++ test/test1.js
@@ -13,21 +13,21 @@ router.post('/reg', function(req, res){
 	if (req.body['repassword'] != req.body['password']) {
 		reJson.success = 0;
 		reJson.errmsg = '两次输入的口令不一致';
-		req.end(reJson.toString());
+		res.send(reJson.toString());
 	} else if (req.body['password'] == '') {
 		reJson.success = 0;
 		reJson.errmsg = '密码不能为空';
-		req.end(reJson.toString());
+		res.send(reJson.toString());
 	} else if (req.body['username'].length < 3) {
 		reJson.success = 0;
 		reJson.errmsg = '用户名小于3个字符';
-		req.end(reJson.toString());
+		res.send(reJson.toString());
 	} else {
 		User.getUserByName(req.body['username'], function(err, userT) {
 			if (!err || userT) {
 				reJson.success = 0;
 				reJson.errmsg = '用户名已存在';
-				req.end(reJson.toString());
+				res.send(reJson.toString());
 			} else {
 				var newUser = new User({
 					name: req.body['username'],
@@ -38,11 +38,11 @@ router.post('/reg', function(req, res){
 					if (err) {
 						reJson.success = 0;
 						reJson.errmsg = err.toString();
-						req.end(reJson.toString());
+						res.send(reJson.toString());
 					} else {
 						req.session.user = userT;
 						reJson.success = 1;
-						req.end(reJson.toString());
+						res.send(reJson.toString());
 					}
 				});
 				
