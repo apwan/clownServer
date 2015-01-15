0fb319987d950d548c0f7186a9fa13eab5935226
apwan06 <471369331@qq.com>
2015-01-15 14:40:36 +0800

    add grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+/**
+ * Created by WuYijie on 1/15/15.
+ */
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+//our JSHint options
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                newcap: true,
+                noarg: true,
+                sub: false,
+                undef: true,
+                unused: false,
+                boss: true,
+                node: true
+            },
+            globals: {
+                exports: true
+            },
+            all: ['ctrl/db.js', 'ctrl/sc.js']
+
+        },
+//our concat options
+        concat: {
+            options: {
+                separator: ';' //separates scripts
+            },
+            dist: {
+                src: ['public/javascripts/lib/modern.js','public/javascripts/lib/application.js', 'public/javascripts/front/SL.js', 'public/javascripts/lib/acetex.js',
+                    'public/javascripts/front/SLeditor.js'], //Grunt mini match for your scripts to concatenate
+                dest: 'public/js/front/clown.js' //where to output the script
+            }
+        },
+//our uglify options
+        uglify: {
+            build: {
+                files: {
+                    'public/js/front/clown.min.js': ['public/js/front/clown.js'] //save over the newly created script
+                }
+            }
+        }
+    });
+//load our tasks
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+// default tasks to run
+    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('development', ['jshint']);
+    grunt.registerTask('production', ['concat', 'uglify']);
+};
